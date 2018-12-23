2e72d1ed12822e39bdf02de80faac7b6e1b4fe4c
Subham Kumar <shubham.kumar.ggps@gmail.com>
2018-12-23 20:14:25 +0530

    Updates

---
+++ precache-manifest.2a73764a7a5bec0c400d8e3d8ef70ced.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "bee1c98fbd1194da2190",
+    "url": "/tic-tac-toe/static/js/main.bee1c98f.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/tic-tac-toe/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "bee1c98fbd1194da2190",
+    "url": "/tic-tac-toe/static/css/main.16cf5a78.chunk.css"
+  },
+  {
+    "revision": "2e2bcc055f601a7f9ee020bde89fac70",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
